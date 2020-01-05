749c984108b1afadf38ae311d9d3f7bb0dae2139
andrehogan <Andre.Hogan001@acad.tri-c.edu>
2020-01-05 14:38:56 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
+import pygame
+from pygame.locals import *
 
+
+def main():
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+
+
+if __name__ == '__main__':
+    main()
